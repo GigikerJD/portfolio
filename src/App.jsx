02ca5b747eaf9e99c69555f2954b487d43f7fc77
GigikerJD <nelgie.ferrol94@gmail.com>
2025-02-26 18:29:12 +0100

    correction App rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./pages/Welcome";
 import { Projects } from "./pages/Projects";
 import { Skills } from "./pages/Skills";
 import { About } from "./pages/About";
 import { Contacts } from "./pages/Contacts";
-
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
-export const App = ({children}) => {
-
-  return(
+export const App = ({ children }) => {
+  return (
     <>
-      <Routes>
-        <Route path="/" element={<Welcome/>} />
-        <Route path="/projects" element={<Projects/>} />
-        <Route path="/skills" element={<Skills/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contacts" element={<Contacts/>} />
-      </Routes>
-
-      <Header/>
-      <div style={{
-        flex: '0 0 auto'
-      }}>
+      <Header />
+      <div style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          {/* Add a fallback route */}
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
         {children}
       </div>
-      <Footer/>
+      <Footer />
     </>
-  )
-}
+  );
+};
